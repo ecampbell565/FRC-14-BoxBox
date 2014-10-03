eca47afd2b623e24e5d33c76debe112b26a74344
Ryan <ryan@luciaonline.net>
2014-10-03 17:04:08 -0400

    Forgot to initialize the buttons

--- src/robot/user_interface.cpp
+++ src/robot/user_interface.cpp
@@ -6,6 +6,9 @@
 Joystick *UI::Primary_Driver::left_stick = new Joystick(1);
 Joystick *UI::Primary_Driver::right_stick = new Joystick(2);
 
+JoystickButton *UI::Primary_Driver::outtake_forward_button = 0;
+JoystickButton *UI::Primary_Driver::outtake_backward_button = 0;
+
 void UI::initialize() {	
 	Primary_Driver::outtake_forward_button = new JoystickButton(Primary_Driver::left_stick, 1);
 	Primary_Driver::outtake_backward_button = new JoystickButton(Primary_Driver::right_stick, 2);
